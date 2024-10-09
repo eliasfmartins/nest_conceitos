9da131498cfb3b76a33e5f90a0e4387649db0f4b
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-10-09 14:42:17 -0300

    feat: alterando importações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
 
 @Module({
-  imports: [],
+  imports: [ConceitosAutomaticoModule],
   controllers: [AppController],
   providers: [AppService],
 })
