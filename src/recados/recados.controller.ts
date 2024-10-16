91e9532acb2fb3c9cf6fa8af835de5049532db06
Elias F Martins <eliasmartinsdev8@gmail.com>
2024-10-16 12:08:10 -0300

    feat: decorators de status code

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
+	@HttpCode(201)
 	@Get()
 	findAll() {
 		return 'Estamos trabalhando nisso "-" '
@@ -9,5 +10,9 @@ export class RecadosController {
 	@Get(':id')
 	findOne(@Param('id') id: string) {
 		return `essa rota deve retornar ${id} only one result`
+	}
+	@Post()
+	create(@Body() body: any) {
+		return body
 	}
 }
